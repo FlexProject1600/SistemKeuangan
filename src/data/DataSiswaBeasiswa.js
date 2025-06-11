62d52a8270a937a85ccc478e9686a4161def8aec
MuhammadFaridBayuHadi.2200016048 <155115946+FaridBayu@users.noreply.github.com>
2025-06-11 14:42:51 +0700

    Delete src directory

--- src/data/DataSiswaBeasiswa.js
+++
@@ -1,8 +0,0 @@
-const DataSiswaBeasiswa = [
-  { nisn: "1234", name: "Agus Rahman", kelas: "8A", beasiswa: "Prestasi Akademik" },
-  { nisn: "8001", name: "Nur Azizah", kelas: "8A", beasiswa: "Yatim Piatu" },
-  { nisn: "8003", name: "Nina Anggraini", kelas: "8A", beasiswa: "Tidak Mampu" },
-  { nisn: "8019", name: "Jihan Maulani", kelas: "8B", beasiswa: "Beasiswa Santri Prestasi" },
-];
-
-export default DataSiswaBeasiswa;
--- src/page/Desktop.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Desktop() {
-  return (
-    <h2>ini akan di arahkan kehalaman desktop setelah login</h2>
-  )
-}
-
-export default Desktop
